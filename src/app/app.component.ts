e68e36f7a515e6efa85cf4cf48f690f7ee7c7946
Srijan <srijan@localhost.localdomain>
2018-04-15 05:29:03 +0530

    All kendos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  dropDown:Array<any>=[{
+  	id:1,
+  	text:"Option one"
+  },
+  {
+  	id:2,
+  	text:"Option two"
+  },
+  {
+  	id:3,
+  	text:"Option three"
+  }];
+
+  years:Array<Number>=[2012,2015,2018];
+  students=[{
+  	name:"Srijan",
+  	results:[100,90,80,70]
+  },
+  {
+  	name:"Paul",
+  	results:[50,60,70,80]
+  },
+  {
+  	name:"Bob",
+  	results:[30,80,10,90]
+  },
+  ];
+
+  fdate:Date=new Date(1998, 2, 24);
+
+  itemWasClicked(event){
+  	console.log(event)
+  }
+
+  buttonWasClicked(){
+  	console.log("Button was clicked")
+  }
+
+  chartItemClicked(event){
+  	console.log(event)
+  }
+
+  logValue(date:Date){
+  	console.log(date);
+  }
+
+  onBlur(){
+  	console.log("Blur");
+  }
+  
+  onFocus(){
+  	console.log("Focus");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { CalendarModule } from '@progress/kendo-angular-dateinputs';
 
 import { AppComponent } from './app.component';
 
+import "hammerjs";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonsModule,
+    ChartsModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
